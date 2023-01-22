2f75e9d2a278edf2856ad7f4d50b8107305906cc
Rafalko, Maksim <maks.rafalko@gmail.com>
2023-01-22 12:29:21 +0300

    refactor: update README.md, add logs even for production env

--- src/logger.ts
+++ src/logger.ts
@@ -1,15 +1,11 @@
 const debug = (message: string): void => {
-    if (process.env['NODE_ENV'] !== 'production') {
-        // eslint-disable-next-line no-console
-        console.log(message);
-    }
+    // eslint-disable-next-line no-console
+    console.log(message);
 };
 
 const error = (message: string): void => {
-    if (process.env['NODE_ENV'] !== 'production') {
-        // eslint-disable-next-line no-console
-        console.error(message);
-    }
+    // eslint-disable-next-line no-console
+    console.error(message);
 };
 
 export { debug, error };
